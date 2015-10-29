36f7b9a0515a33d00928add307df23d78051ef41
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-10-29 10:07:02 -0400

    notes from in class review and previous button

--- src/People.java
+++ src/People.java
@@ -49,21 +49,32 @@ public class People {
                         namesStart = Integer.valueOf(offset);
                     }
 
-                    if(!(namesStart<people.size())) {
-                        Spark.halt(403);
-                    }
+//                    if(!(namesStart<people.size())) {
+//                        Spark.halt(403);
+//                    }
+//                    ArrayList<Person> peeps = new ArrayList<Person>(people.subList(namesStart, namesStart + SHOW_COUNT));
 
-                    ArrayList<Person> peeps = new ArrayList<Person>(people.subList(namesStart, namesStart + SHOW_COUNT));
-                    //ArrayList<Person> peeps = new ArrayList<Person>(people.subList(Math.min(people.size(), namesStart, Math.min(people.size(), namesStart + 20));
-                    //this will eliminate an error if "http://localhost:4567/?offset=990" this is entered into the URL or if we go over people.size();
-                    //this could replace the if(!(namesStart<people.size())) {Spark.halt(403);} above.
+                    ArrayList<Person> peeps = new ArrayList<Person>(people.subList(
+                            Math.max(0, Math.min(people.size(), namesStart)),
+                            Math.max(0, Math.min(people.size(), namesStart + SHOW_COUNT))
+                    ));
+
+                    //^this will eliminate an error if "http://localhost:4567/?offset=990" this is entered into the URL or if we go over people.size();
+                    //this replaces the:
+                    // ArrayList<Person> peeps = new ArrayList<Person>(people.subList(namesStart, namesStart + SHOW_COUNT));
+                    // if(!(namesStart<people.size())) {Spark.halt(403);} above.
+                    //this will say first, make sure it's not too big and then make sure it's not too small.
 
                     HashMap m = new HashMap();
                     m.put("people", peeps); //pass ArrayList: peeps into the hashmap to show in the html where we call "people"
+                    m.put("oldOffset", namesStart - SHOW_COUNT); //"oldOffset" calls {{oldOffset}} from people.html
                     m.put("newOffset", namesStart + SHOW_COUNT); //"newOffset" calls {{newOffset}} from people.html
 
                     boolean showNext = namesStart + SHOW_COUNT < people.size();
                     m.put("showNext", showNext); //will stop showing the "next" button when there will be no more names to display on another page
+
+                    boolean showPrev = namesStart > 0 ;
+                    m.put("showPrev", showPrev);
 
                     return new ModelAndView(m, "people.html");
                 }),
